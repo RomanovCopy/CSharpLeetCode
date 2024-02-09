8e878e580a39bc90ccf78f5a7ee65555c584cdb4
Сергей Романов <RomanovCopy@gmail.com>
2024-02-09 15:03:01 +0300

    ready

--- MyLeetCodeProjects/LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ MyLeetCodeProjects/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace LongestSubstringWithoutRepeatingCharacters
 {
+    /*
+     * Учитывая строку s, найдите длину самой длинной подстроки без повторяющихся символов.
+
+Пример 1:
+Ввод: s = "abcabcbb"
+Вывод: 3
+Объяснение: Ответ - "abc", длиной 3.
+
+Пример 2:
+Ввод: s = "bbbbb"
+Вывод: 1
+Объяснение: Ответ - "b", длиной 1.
+
+Пример 3:
+Ввод: s = "pwwkew"
+Вывод: 3
+Объяснение: Ответ - "wke", длиной 3.
+
+Обратите внимание, что ответ должен быть подстрокой, "pwke" - это подпоследовательность, а не подстрока."
+     */
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            new Solution();
         }
     }
 }
---
+++ MyLeetCodeProjects/LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstringWithoutRepeatingCharacters
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+            LengthOfLongestSubstring("abcabcbb");
+        }
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            int length = 0;
+            string line = "";
+            for(int i = 0; i < s.Length; i++)
+            {
+                line = s[i].ToString();
+                for(int j = i; j < s.Length; j++)
+                {
+                    if (j > i)
+                    {
+                        if (!line.Contains(s[j]))
+                        {
+                            line = line + s[j];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                length = line.Length > length ? line.Length : length;
+            }
+
+            return length;
+        }
+
+
+
+
+    }
+}
