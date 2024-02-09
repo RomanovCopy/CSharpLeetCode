e291eb4f5f90a6f84a0a5b864ba081729391974a
Сергей Романов <RomanovCopy@gmail.com>
2024-02-09 14:09:04 +0300

    Ready

--- MyLeetCodeProjects/002AddTwoNumbers/Solution.cs
+++ MyLeetCodeProjects/002AddTwoNumbers/Solution.cs
@@ -11,48 +11,109 @@ namespace _002AddTwoNumbers
 
         public Solution()
         {
-            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3, null)));
-            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4, null)));
+            ListNode l1 = CreateListNode(new int[] { 2,4,3 });
+            ListNode l2 = CreateListNode(new int[] { 5,6,4 });
             AddTwoNumbers(l1, l2);
         }
 
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            ListNode l3 = new ListNode();
-            ListNode l = l3;
+            string value1 = ListNodeToString(l1);
+            string value2 = ListNodeToString(l2);
+            string value = NumbersStringToNumbersString(value1, value2);
+            return CreateListNode(StringToIntArray(value));
+        }
+
+        private string NumbersStringToNumbersString(string val1, string val2)
+        {
+            string value = "";
             int transfer = 0;
-            while (l1 != null || l2 != null)
+            if (val2.Length > val1.Length)
             {
-                int value = l1.val + l2.val + transfer;
-                l3.next = new ListNode(0);
-                if (value > 9)
+                string temp = val1;
+                val1 = val2;
+                val2 = temp;
+            }
+            for (int i = 0; i < val1.Length; i++)
+            {
+                int v1 = int.Parse(val1[i].ToString());
+                int v2;
+                if (val2.Length > i)
                 {
-                    transfer = value / 10;
-                    value = value % 10;
+                    v2 = int.Parse(val2[i].ToString());
+                }
+                else { v2 = 0; }
+                int res = v1 + v2 + transfer;
+                if (res > 9)
+                {
+                    transfer = res / 10;
+                    value =value + res % 10;
                 }
                 else
                 {
                     transfer = 0;
+                    value = value + res;
                 }
-                l3.val = value;
-                l3 = l3.next;
-                l1 = l1.next;
-                l2 = l2.next;
             }
-            return l;
-        }
-
-
-        public class ListNode
-        {
-            public int val;
-            public ListNode next;
-            public ListNode(int val = 0, ListNode next = null)
+            if (transfer > 0)
             {
-                this.val = val;
-                this.next = next;
+                value = value + transfer;
             }
+            return value;
         }
 
+    private string ListNodeToString(ListNode listNode)
+    {
+        string value = "";
+        while (listNode != null)
+        {
+            value = value + listNode.val.ToString();
+            listNode = listNode.next;
+        }
+        return value;
     }
+
+    private int[] StringToIntArray(string value)
+    {
+        int[] array = new int[value.Length];
+        for (int i = 0; i < value.Length; i++)
+        {
+            array[i] = int.Parse(value[i].ToString());
+        }
+        return array;
+    }
+
+    private ListNode CreateListNode(int[] values)
+    {
+        ListNode listNode = new ListNode();
+        ListNode newListNode = listNode;
+        if (values.Length > 0)
+        {
+            listNode.val = values[0];
+            if (values.Length > 1)
+            {
+                for (int i = 1; i < values.Length; i++)
+                {
+                    listNode.next = new ListNode();
+                    listNode.next.val = values[i];
+                    listNode = listNode.next;
+                }
+            }
+        }
+        return newListNode;
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
 }
