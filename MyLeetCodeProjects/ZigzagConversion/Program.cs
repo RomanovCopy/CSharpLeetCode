529dd5a6bcadeff76a2da9fc94dd0f121c048ab8
RomanovCopy <RomanovCopy@gmail.com>
2024-02-11 20:41:03 +0300

    fix1102242040

--- MyLeetCodeProjects/ZigzagConversion/Program.cs
+++ MyLeetCodeProjects/ZigzagConversion/Program.cs
@@ -6,6 +6,35 @@ using System.Threading.Tasks;
 
 namespace ZigzagConversion
 {
+    /*
+     ```plaintext
+    Строка "PAYPALISHIRING" записывается в шахматном порядке на заданном количестве строк вот так: 
+        (возможно, вам следует отобразить этот шаблон в фиксированным шрифтом для лучшей читаемости)
+
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+    Затем прочитайте построчно: "PAHNAPLSIIGYIR"
+
+    Напишите код, который примет строку и сделает эту конвертацию для заданного количества строк:
+
+    string convert(string s, int numRows);
+ 
+    Пример 1:
+
+    Ввод: s = "PAYPALISHIRING", numRows = 3
+    Вывод: "PAHNAPLSIIGYIR"
+    Пример 2:
+
+    Ввод: s = "PAYPALISHIRING", numRows = 4
+    Вывод: "PINALSIGYAHRPI"
+    Пояснение:
+    P     I    N
+    A   L S  I G
+    Y A   H R
+    P     I
+    ```
+     */
     internal class Program
     {
         static void Main(string[] args)
