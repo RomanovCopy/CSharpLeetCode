df9be3dfc5292b7e174de89efe80e78c8a388196
RomanovCopy <RomanovCopy@gmail.com>
2024-02-11 12:17:06 +0300

    fix1112241216

--- MyLeetCodeProjects/005LongestPalindromicSubstring/Program.cs
+++ MyLeetCodeProjects/005LongestPalindromicSubstring/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace _005LongestPalindromicSubstring
 {
+    /*Учитывая строку s, необходимо найти самую длинную палиндромную подстроку в s.
+
+    Пример 1:
+    Ввод: s = "babad"
+    Вывод: "bab"
+    Пояснение: "aba" также является допустимым ответом.
+
+    Пример 2:
+    Ввод: s = "cbbd"
+    Вывод: "bb"*/
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            new Solution();
         }
     }
 }
