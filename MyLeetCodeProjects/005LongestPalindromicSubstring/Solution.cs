8b6d20c8624be74794306c154bca9d4a1aa8169d
RomanovCopy <RomanovCopy@gmail.com>
2024-02-11 13:28:08 +0300

    fix1102241328

--- MyLeetCodeProjects/005LongestPalindromicSubstring/Solution.cs
+++ MyLeetCodeProjects/005LongestPalindromicSubstring/Solution.cs
@@ -8,8 +8,60 @@ namespace _005LongestPalindromicSubstring
 {
     internal class Solution
     {
+        string line = "babad";
         public Solution()
         {
+            Console.WriteLine(LongestPalindrome(line));
+            Console.ReadKey();
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            string polindrom = "";
+            string[] words = s.Split(' ');
+            foreach(var word in words)
+            {
+                string w = word.Trim();
+                if (IsPalindrom(w) && w.Length>polindrom.Length)
+                    polindrom = w;
+            }
+            return polindrom;
+        }
+
+        private bool IsPalindrom(String word)
+        {
+            int lenght = word.Length;
+            if (lenght > 1)
+            {
+                int startBegin = 0;
+                int endBegin = lenght - 1;
+                for (; startBegin<endBegin;)
+                {
+                    for (; startBegin<endBegin;)
+                    {
+                        if (!char.IsLetterOrDigit(word[startBegin]))
+                        {
+                            startBegin++;
+                            break;
+                        }else if (!char.IsLetterOrDigit(word[endBegin]))
+                        {
+                            endBegin--;
+                            break;
+                        }
+                        else
+                        {
+                            if (! (word[startBegin] == word[endBegin]))
+                            {
+                                return false;
+                            }
+                                startBegin++;
+                                endBegin--;
+                        }
+                    }
+                }
+
+            }
+            return true;
         }
     }
 }
