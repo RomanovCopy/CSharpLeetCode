29ea45f4b10e4019ebc35fe15c0a9cfa3115361f
Сергей Романов <RomanovCopy@gmail.com>
2024-02-12 15:08:37 +0300

    Start

---
+++ MyLeetCodeProjects/008StringToInteger(atoi)/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008StringToInteger_atoi_
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+        }
+
+        public int MyAtoi(string s)
+        {
+
+        }
+
+
+    }
+}
+}
