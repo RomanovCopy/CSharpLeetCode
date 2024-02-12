bf6e52d1a319d258a615016384bd708ed5ec6ffb
Сергей Романов <RomanovCopy@gmail.com>
2024-02-12 13:45:06 +0300

    Start

---
+++ MyLeetCodeProjects/007ReverseInteger/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007ReverseInteger
+{
+    internal class Program
+    {
+        /*
+    Дано знаковое 32-битное целое число x, вернуть x с перевернутыми цифрами. 
+        Если обращение x заставляет значение выходить за пределы знакового 32-битного диапазона целых чисел [-231, 231 - 1],
+        то вернуть 0.
+
+    Предположим, что среда не позволяет вам хранить 64-битные целые числа (со знаком или без).
+
+    Пример 1:
+
+        Ввод: x = 123
+        Вывод: 321
+
+    Пример 2:
+
+        Ввод: x = -123
+        Вывод: -321
+
+    Пример 3:
+
+        Ввод: x = 120
+        Вывод: 21
+         */
+
+        static void Main(string[] args)
+        {
+            new Solution();
+        }
+    }
+}
---
+++ MyLeetCodeProjects/007ReverseInteger/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007ReverseInteger
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+            Console.WriteLine(Reverse(125));
+            Console.ReadKey();
+        }
+
+        public int Reverse(int x)
+        {
+
+        }
+    }
+}
