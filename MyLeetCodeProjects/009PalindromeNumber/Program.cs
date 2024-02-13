d747909e23d355757ac028a7892dd5df56e5610f
Сергей Романов <RomanovCopy@gmail.com>
2024-02-13 12:31:27 +0300

    Ready

---
+++ MyLeetCodeProjects/009PalindromeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009PalindromeNumber
+{
+    /*
+     Учитывая целое число x, верните true, если x является палиндромом, и false в противном случае.
+
+    Пример 1:
+
+    Входные данные: x = 121
+    Результат: true
+    Объяснение: 121 читается как 121 слева направо и справа налево.
+
+    Пример 2:
+
+    Входные данные: x = -121
+    Результат: false
+    Объяснение: Слева направо читается -121. Справа налево становится 121-. Поэтому это не палиндром.
+
+    Пример 3:
+
+    Входные данные: x = 10
+    Результат: false
+    Объяснение: Читается как 01 справа налево. Поэтому это не палиндром.
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Solution();
+        }
+    }
+}
---
+++ MyLeetCodeProjects/009PalindromeNumber/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009PalindromeNumber
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+            Console.WriteLine( IsPalindrome(1221));
+            Console.ReadKey();
+        }
+
+        public bool IsPalindrome(int x)
+        {
+            //исключительные варианты
+            if (x < 0 || (x % 10 == 0 && x != 0))
+                return false;
+            int revertedNumber = 0;//реверсивное число
+            //переводим в реверсивное только правую половину исходного числа
+            while (x > revertedNumber)
+            {
+                //ставим последнюю цифру исходного числа в начало реверсивного
+                revertedNumber = revertedNumber * 10 + x % 10;
+                //удаляем последнюю цифру исходного числа
+                x /= 10;
+            }
+            //если цифр четное колличество, то x == revertedNumber
+            //иначе x == revertedNumber / 10
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
