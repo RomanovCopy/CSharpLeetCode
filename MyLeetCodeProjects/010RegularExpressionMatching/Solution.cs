a5357102a4f16a413b939efd00fb42aa9000e3c1
Сергей Романов <RomanovCopy@gmail.com>
2024-02-13 12:39:52 +0300

    Start

---
+++ MyLeetCodeProjects/010RegularExpressionMatching/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010RegularExpressionMatching
+{
+    internal class Solution
+    {
+        public Solution()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s">входное число</param>
+        /// <param name="p">шаблон регулярного выражения</param>
+        /// <returns></returns>
+        public bool IsMatch(string s, string p)
+        {
+
+        }
+    }
+}
