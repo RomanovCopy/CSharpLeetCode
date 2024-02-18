b540953e994ed71281f1f88629f33abc361a6c40
RomanovCopy <RomanovCopy@gmail.com>
2024-02-18 20:49:34 +0300

    fix1802242050

--- MyLeetCodeProjects/011ContainerWithMostWater/Solution.cs
+++ MyLeetCodeProjects/011ContainerWithMostWater/Solution.cs
@@ -10,6 +10,37 @@ namespace _011ContainerWithMostWater
     {
         public Solution()
         {
+            int[] height = { 2, 3, 4, 5, 18, 17, 6 };
+            Console.WriteLine( MaxArea(height));
+            Console.ReadKey();
+        }
+
+        public int MaxArea(int[] height)
+        {
+            int length = height.Length;
+            int[,] dp = new int[length, length];
+            int maxArea = 0;
+            if (length == 2)
+            {
+                return Math.Min(height[0], height[1]);
+            }
+
+            for (int left = 0; left < length - 1; left++)
+            {
+                for (int right = left + 1; right < height.Length; right++)
+                {
+                    if (left + 1 < right)
+                    {
+                        dp[left, right] = Math.Min(height[left], height[right]) * (right - left);
+                        dp[left, right] = Math.Max(dp[left, right], dp[left + 1, right]);
+                        dp[left, right] = Math.Max(dp[left, right], dp[left, right - 1]);
+                    }
+
+                    maxArea = Math.Max(maxArea, dp[left, right]);
+                }
+            }
+
+            return maxArea;
         }
     }
 }
