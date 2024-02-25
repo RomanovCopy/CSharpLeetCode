e07279e559488f139ece2bef92839ec1ac1c0f05
RomanovCopy <RomanovCopy@gmail.com>
2024-02-25 16:54:55 +0300

    End

--- MyLeetCodeProjects/011ContainerWithMostWater/Solution.cs
+++ MyLeetCodeProjects/011ContainerWithMostWater/Solution.cs
@@ -8,36 +8,40 @@ namespace _011ContainerWithMostWater
 {
     internal class Solution
     {
-        public Solution()
+        public Solution ( )
         {
-            int[] height = { 2, 3, 4, 5, 18, 17, 6 };
-            Console.WriteLine( MaxArea(height));
-            Console.ReadKey();
+            int[ ] height = { 2 , 2 };
+            Console.WriteLine( MaxArea( height ) );
+            Console.ReadKey( );
         }
 
-        public int MaxArea(int[] height)
+        public int MaxArea ( int[ ] heights )
         {
-            int length = height.Length;
-            int[,] dp = new int[length, length];
             int maxArea = 0;
-            if (length == 2)
+            int length = heights.Length;
+            if ( length > 1 )
             {
-                return Math.Min(height[0], height[1]);
-            }
-
-            for (int left = 0; left < length - 1; left++)
-            {
-                for (int right = left + 1; right < height.Length; right++)
+                int left = 0;
+                int right = length - 1;
+                while ( left < right )
                 {
-                    if (left + 1 < right)
+                    int width = Math.Abs( left - right );
+                    int height = Math.Min( heights[ left ] , heights[ right ] );
+                    int area = width * height;
+                    maxArea = area > maxArea ? area : maxArea;
+                    if(heights[ left ] < heights[ right ] )
                     {
-                        dp[left, right] = Math.Min(height[left], height[right]) * (right - left);
-                        dp[left, right] = Math.Max(dp[left, right], dp[left + 1, right]);
-                        dp[left, right] = Math.Max(dp[left, right], dp[left, right - 1]);
+                        left++;
+                    } else if(heights[ right ] < heights[ left ])
+                    {
+                        right--;
+                    } else
+                    {
+                        left++;
+                        right--;
                     }
-
-                    maxArea = Math.Max(maxArea, dp[left, right]);
                 }
+
             }
 
             return maxArea;
