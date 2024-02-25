d581af5e76aabdc573e5179e4a23c7f8ffe6a42a
RomanovCopy <RomanovCopy@gmail.com>
2024-02-25 18:00:47 +0300

    fix2502241800

--- MyLeetCodeProjects/012IntegertoRoman/Solution.cs
+++ MyLeetCodeProjects/012IntegertoRoman/Solution.cs
@@ -8,5 +8,28 @@ namespace _012IntegertoRoman
 {
     internal class Solution
     {
+        private Dictionary<int , String> intToRoman = new Dictionary<int , string>( )
+        {
+            {1,"I" }, {2, "II" }, {3,"III" },{4,"IV"},{5,"V"},{6,"VI"},{7,"VII"},{8,"VIII"},
+            {9,"IX" },{10,"X"},{50,"L"},{100,"C"},{500,"D"}, {1000,"M"}
+        };
+
+        private int[ ] multipliers = { 1000 , 500 , 100 , 50 , 10 , 5, 1 };
+
+        public Solution ( )
+        {
+            Console.WriteLine( IntToRoman( 56 ) );
+        }
+
+        public string IntToRoman ( int num )
+        {
+            string roman = "";
+
+            
+
+            return roman;
+        }
+
+
     }
 }
