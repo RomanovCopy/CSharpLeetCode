44b426ae2816242aa8884b3f6d526d92b29edf60
Сергей Романов <RomanovCopy@gmail.com>
2024-03-07 12:00:20 +0300

    Start

---
+++ MyLeetCodeProjects/014LongestCommonPrefix/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _014LongestCommonPrefix
+{
+    /*Напишите функцию для поиска самого длинного общего префикса среди массива строк.
+
+    Если общего префикса нет, верните пустую строку "".
+
+    Пример 1:
+
+        Input: strs = ["flower","flow","flight"]
+        Output: "fl"
+    Пример 2:
+
+        Input: strs = ["dog","racecar","car"]
+        Output: ""
+        Объяснение: Среди входных строк нет общего префикса.
+      Ограничения:
+
+            1 <= strs.length <= 200
+            0 <= strs[i].length <= 200
+       strs[i] состоит только из строчных букв английского алфавита.*/
+
+    internal class Program
+    {
+
+
+        static void Main ( string[ ] args )
+        {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
