38d2d2d2604549f303269b161c545740e52ad277
Сергей Романов <RomanovCopy@gmail.com>
2024-03-07 11:52:06 +0300

    End

--- MyLeetCodeProjects/013RomanToInteger/Solution.cs
+++ MyLeetCodeProjects/013RomanToInteger/Solution.cs
@@ -8,13 +8,34 @@ namespace _013RomanToInteger
 {
     internal class Solution
     {
+        private readonly Dictionary<string , int> romaNumbersToInteger = new Dictionary<string , int>( )
+        {
+            {"M",1000 },{"CM",900},{"D",500},{"CD",400},{"C",100},{ "XC",90},{"L",50},{"XL",40},{"X",10},
+            {"IX",9 },{"IV",4},{"V",5},{"I",1}
+
+        };
+
+        
+
         public Solution ( )
         {
+            Console.WriteLine( RomanToInt( "MMXXIV" ));
         }
 
         public int RomanToInt ( string s )
         {
-
+            if(s.Length>0)
+            {
+                foreach(string key in romaNumbersToInteger.Keys )
+                {
+                    if ( s.Contains( key )&& s.IndexOf(key)==0 )
+                    {
+                        s = s.Remove( 0 , key.Length );
+                        return romaNumbersToInteger[ key ] + RomanToInt( s );
+                    }
+                }
+            }
+            return 0;
         }
     }
 }
