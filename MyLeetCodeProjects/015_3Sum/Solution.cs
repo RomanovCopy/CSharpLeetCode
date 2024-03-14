71ef16c15d081e3c7ab46a9c49ed9df9c95987e1
Сергей Романов <RomanovCopy@gmail.com>
2024-03-14 15:38:58 +0300

    Start

---
+++ MyLeetCodeProjects/015_3Sum/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_3Sum
+{
+    internal class Solution
+    {
+        public Solution ( )
+        {
+
+        }
+
+        public IList<IList<int>> ThreeSum ( int[ ] nums )
+        {
+
+        }
+    }
+}
